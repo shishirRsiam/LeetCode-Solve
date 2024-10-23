ec6638960ca77f2e9e67b0b2c4f0f0ce00b41097
shishirRsiam <sishir.rahman01@gmail.com>
2024-10-23 10:48:27 +0600

    23 Oct 2024

---
+++ 993. Cousins in Binary Tree (DFS).cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int target1, target2;
+    map<int, vector<int>>store;
+    void solve(TreeNode* root, int depth)
+    {
+        if(!root) return;
+        if(root->left and (root->left->val == target1 or root->left->val == target2))
+            store[depth].push_back(root->val);
+        if(root->right and (root->right->val == target1 or root->right->val == target2))
+            store[depth].push_back(root->val);
+            
+        solve(root->left, depth + 1);
+        solve(root->right, depth + 1);
+    }
+    bool isCousins(TreeNode* root, int x, int y) 
+    {
+        target1 = x, target2 = y;
+        solve(root, 1);
+        for(auto [depth, vec]:store)
+        {
+            int n = vec.size();
+            for(int i = 0; i < n; i++)
+                for(int j = i + 1; j < n; j++)
+                    if(vec[i] != vec[j]) return true;
+            cout<<"Depth "<<depth<<" -> "; 
+            for(auto val:vec) cout<<val<<" ";
+            cout<<endl;  
+        }
+        return false;
+    }
+};
