6ce5447cceb84118857088a6c94747b31ca7dbc4
shishirRsiam <shishir.siam01@gmail.com>
2025-01-14 10:51:56 +0600

    1010. Pairs of Songs With Total Durations Divisible by 60

--- 1010. Pairs of Songs With Total Durations Divisible by 60.cpp
+++ 1010. Pairs of Songs With Total Durations Divisible by 60.cpp
@@ -13,3 +13,22 @@ public:
         return ans + ((count[0] * long(count[0] - 1)) / 2);
     }
 };
+
+
+class Solution {
+public:
+    int numPairsDivisibleBy60(vector<int>& time) 
+    {
+        vector<int> count(65);
+        int ans = 0, n = time.size(), mod;
+        for(int i = 0; i < n; i++)
+        {
+            mod = time[i] % 60;
+            if(mod == 0) ans += count[mod];
+            else ans += count[60 - mod];
+            // ans += count[(mod == 0 ? 0 : 60) - mod];
+            count[mod]++;
+        }
+        return ans;
+    }
+};
