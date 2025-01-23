714e159e305b5b8be4eb72f8dd51baadb62df254
shishirRsiam <shishir.siam01@gmail.com>
2025-01-23 10:34:10 +0600

    1267. Count Servers that Communicate

---
+++ 1267. Count Servers that Communicate.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countServers(vector<vector<int>>& grid) 
+    {
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<int>> store(2, vector<int>(255));
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
+                store[0][i] += grid[i][j], store[1][j] += grid[i][j];
+
+        int ans = 0;
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
+                ans += grid[i][j] and (store[0][i] > 1 or store[1][j] > 1);
+
+        return ans;    
+    }
+};
