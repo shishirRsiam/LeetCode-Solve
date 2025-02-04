bbc96bfe07292331f56567451bc13069a9605e08
shishirRsiam <shishir.siam01@gmail.com>
2025-02-04 11:01:58 +0600

    1800. Maximum Ascending Subarray Sum

---
+++ 1800. Maximum Ascending Subarray Sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) 
+    {
+        int n = nums.size(), sum = nums[0], maxSum = nums[0];   
+        for(int i = 1; i < n; i++)
+        {
+            if(nums[i] <= nums[i - 1]) sum = 0;
+            sum += nums[i];
+            maxSum = max(maxSum, sum);
+        }
+        return maxSum;
+    }
+};
