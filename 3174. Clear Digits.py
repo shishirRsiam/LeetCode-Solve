107ec6216e21dd73c96300fbc3eb48a7cc78bbc2
shishirRsiam <shishir.siam01@gmail.com>
2025-02-10 11:13:57 +0600

    3174. Clear Digits

---
+++ 3174. Clear Digits.py
@@ -0,0 +1,7 @@
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        ans = []
+        for ch in s:
+            if ch.isdigit() and ans: ans.pop()
+            else: ans.append(ch)
+        return ''.join(ans)
