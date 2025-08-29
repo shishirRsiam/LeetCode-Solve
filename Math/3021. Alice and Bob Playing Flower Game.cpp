a3f29b89244b9b663ef12a43fa4e584ebe4ecd0e
shishirRsiam <shishir.siam01@gmail.com>
2025-08-29 21:27:53 +0600

    3021. Alice and Bob Playing Flower Game

--- Math/3021. Alice and Bob Playing Flower Game.cpp
+++ Math/3021. Alice and Bob Playing Flower Game.cpp
@@ -10,3 +10,13 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    long long flowerGame(int n, int m) 
+    {
+        long even_count1 = n / 2, odd_count1 = (n + 1) / 2;
+        long even_count2 = m / 2, odd_count2 = (m + 1) / 2;
+        return (even_count1 * odd_count2) + (odd_count1 * even_count2);
+    }
+};
