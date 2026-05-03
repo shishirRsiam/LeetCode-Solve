4ef719cf8dddf33eb020358452908f0577055245
shishirRsiam <shishir.siam01@gmail.com>
2026-05-03 16:16:40 +0600

    796. Rotate String

--- 796. Rotate String.py
+++ 796. Rotate String.py
@@ -2,3 +2,7 @@ class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
         s += s
         return len(s) / 2 == len(goal) and goal in s
+
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        return len(s) == len(goal) and s in goal + goal
