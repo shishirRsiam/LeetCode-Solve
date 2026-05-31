2b69a0f2297222418aa72dee8162bb950e4c042b
shishirRsiam <shishir.siam01@gmail.com>
2026-05-31 21:41:53 +0600

    2126. Destroying Asteroids

---
+++ 2126. Destroying Asteroids.py
@@ -0,0 +1,8 @@
+class Solution:
+    def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
+        asteroids.sort()
+        for ast in asteroids:
+            if ast > mass:
+                return False
+            mass += ast
+        return True
