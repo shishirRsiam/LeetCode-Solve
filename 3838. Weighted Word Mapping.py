42c3d3e479fb9fcd0bbfe120319c57ef1f21f076
shishirRsiam <shishir.siam01@gmail.com>
2026-06-13 22:40:43 +0600

    3838. Weighted Word Mapping

--- 3838. Weighted Word Mapping.py
+++ 3838. Weighted Word Mapping.py
@@ -5,3 +5,12 @@ class Solution:
             index = sum([weights[ord(ch) - ord('a')] for ch in word]) % 26
             ans += store[index]
         return ans
+
+class Solution:
+    def mapWordWeights(self, words: List[str], weights: List[int]) -> str:
+        ans = []
+        reverse_words = ascii_lowercase[::-1]
+        for w in words:
+            sum_ord = sum([weights[ord(ch) - ord('a')] for ch in w])     
+            ans.append(reverse_words[sum_ord % 26])
+        return ''.join(ans)
